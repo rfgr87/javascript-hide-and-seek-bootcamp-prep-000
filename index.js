3501b4a29a742bf840a123b0488bc8aefbd65fd5
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-07 21:43:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function getFirstSelector(selector){
+  const out = document.querySelector(selector);
+  console.log('out', out)
+  return out
+}
 
+function nestedTarget(){
+  const out = document.querySelector('#nested').querySelector('.target')
+  console.log('out', out)
+  return out
+}
+
+
+function increaseRankBy(n) {
+  var allNumbers = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < allNumbers.length; i++) {
+    allNumbers[i].innerHTML = parseInt(allNumbers[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var deepestChild;
+  deepestChild = document.querySelector('#grand-node div div div div')
+  return deepestChild
+}
+
+//ANOTHER SOLUTION
+/*
+function deepestChild() {
+  var grandNode = document.querySelector('#grand-node');
+  var deepestNode = grandNode.children[0];
+  for (var i = 0; !deepestNode.children[i]; i) {
+    deepestNode = deepestNode.children[0]
+  }
+  return deepestNode;
+}
+*/
